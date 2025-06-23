538c1f38d0f3bdbda21baa904ce6814c96329cc3
IvanBY7 <166653417+IvanBY7@users.noreply.github.com>
2025-06-22 22:41:15 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Invitación de Boda</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Swiper CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+<div id="carta-modal">
+  <div class="carta-container">
+    <!-- <img id="carta-imagen" src="img/carta_2.png" alt="Carta animada" /> -->
+     <video id="carta-video" width="300" poster="img/carta_2.jpg" preload="auto" onclick="prueba()">
+      <source src="img/carta_vid.mp4" type="video/mp4">
+      Tu navegador no soporta video HTML5.
+    </video>
+  </div>
+</div>
+
+<div id="contenido-principal" class="contenido_principal " style="display: none;"> 
+  <!-- Portada -->
+  <section class="portada">
+    <div class="contenido-portada">
+      <!-- <img src="img/anillos.png" class="anillos" alt="Anillos" /> -->
+      <!-- <h1>Nuestro amor, en el tiempo perfecto</h1>
+      <p>No necesitamos una eternidad para saberlo… con un instante fue suficiente para entender que queríamos pasarla juntos. 💍</p> -->
+    </div>
+  </section>
+
+  <!-- Presentación -->
+  <section class="presentacion">
+    <div class="custom-audio-player controlador">
+      <h2 class="titulo-audio"> DALE PLAY A NUESTRO <br> COMIENZO </h2>
+      <button id="btnPlayPause" class="btn btn-outline-danger">
+        <img id="imgAudio" src="source/icons/pause.svg" alt="Pausa" width="40" height="40">
+      </button>
+      <h2 class="titulo-audio"> CON ESTA CANCIÓN ESPECIAL </h2>
+      <!-- <span id="statusAudio" style="margin-left: 10px;">Reproduciendo...</span> -->
+    </div>
+    <div class="nombre_presentacion">
+      <h1>SAMANTHA & ALBERTO</h1>
+    </div>
+    <h2 class="titulo_roboto">¡NOS CASAMOS!</h2>
+    <div class="audio-player">
+      <audio class="musica" id="musica_boda" controls preload="auto" loop>
+        <source src="source/music/musica_v2.mp3" type="audio/mpeg">
+          Tu navegador no soporta el audio HTML5.
+      </audio>
+    </div>
+  </section>
+
+  <section class="primera_foto">
+    <div>
+      <h2 class="descripcion_roboto">Nos encantaría que seas parte de este momento tan especial para nosotros</h2>
+    </div>
+    <div class="primera_imagen">
+      <img id="imgAudio" src="img/foto_prueba.jpeg" alt="Imagen juntos" width="70%" height="70%"/>
+    </div>
+  </section>
+  <section class="seccion_calendario">
+    <h2>¡SOLO FALTAN!</h2>
+    <div id="contador" class="contador">
+      <div class="bloque">
+        <div id="dias" class="valor">00</div>
+        <div class="etiqueta">DÍAS</div>
+      </div>
+      <div class="bloque">
+        <div id="dias" class="valor">:</div>
+        <div class="etiqueta"> </div>
+      </div>
+      <div class="bloque">
+        <div id="horas" class="valor">00</div>
+        <div class="etiqueta">HORAS</div>
+      </div>
+      <div class="bloque">
+        <div id="horas" class="valor">:</div>
+        <div class="etiqueta"> </div>
+      </div>
+      <div class="bloque">
+        <div id="minutos" class="valor">00</div>
+        <div class="etiqueta">MINUTOS</div>
+      </div>
+      <div class="bloque">
+        <div id="minutos" class="valor">:</div>
+        <div class="etiqueta"> </div>
+      </div>
+      <div class="bloque">
+        <div id="segundos" class="valor">00</div>
+        <div class="etiqueta">SEGUNDOS</div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Carrusel -->
+  <!-- <section class="carrusel bg-white py-5 text-center">
+    <div class="container">
+      <h2 class="mb-4" style="color: #da6e6e;">Nuestros Recuerdos</h2>
+
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slide">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="https://source.unsplash.com/300x400/?wedding,1" alt="Foto 1" />
+                  <img src="img/foto_1.jpeg" alt="Foto 1" class="img-fluid rounded-4 shadow" />
+                </div>
+                <div class="flip-card-back">
+                  <p>Nuestra primera cita ❤️</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="https://source.unsplash.com/300x400/?wedding,1" alt="Foto 1" />
+                  <img src="img/foto_2.jpeg" alt="Foto 1" class="img-fluid rounded-4 shadow"
+                </div>
+                <div class="flip-card-back">
+                  <p>segunda cita ❤️</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="img/foto_3.jpeg" alt="Foto 1" class="img-fluid rounded-4 shadow" />
+                </div>
+                <div class="flip-card-back">
+                  <p>Nuestra tercera cita ❤️</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="img/foto_1.jpeg" alt="Foto 1" class="img-fluid rounded-4 shadow" />
+                </div>
+                <div class="flip-card-back">
+                  <p>Nuestra cuarta cita ❤️</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="img/foto_1.jpeg" alt="Foto 1" class="img-fluid rounded-4 shadow" />
+                </div>
+                <div class="flip-card-back">
+                  <p>Nuestra quinta cita ❤️</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div> Controles
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+  </section> -->
+
+
+  <!-- Evento Civil -->
+  <section class="lugar_evento">
+    <div class="tarjeta_ubicacion">
+      <div class="filtro"></div> <!-- filtro negro encima del fondo -->
+      <div class="contenido_tarjeta">
+        <img id="imgAudio" src="source/icons/icono_tarjeta.svg" alt="Pausa" width="40" height="40" color="white">
+        <h2>Boda Civil</h2>
+        <p><strong>Ubicación:</strong> Casa KOKAY</p>
+        <p class="link_ubicacion"><a href="https://www.google.com/maps?q=584J+24+Casa+Kokay,+97334+San+Ignacio,+Yuc.&ftid=0x8f55df0014ff1969:0x65abeee0a4a3f87e&entry=gps&lucs=,94274536,94275316,94224825,94227247,94227248,94231188,47071704,47069508,94218641,94203019,47084304&g_ep=CAISEjI1LjI0LjEuNzY5MjczNTU2MBgAIIgnKmMsOTQyNzQ1MzYsOTQyNzUzMTYsOTQyMjQ4MjUsOTQyMjcyNDcsOTQyMjcyNDgsOTQyMzExODgsNDcwNzE3MDQsNDcwNjk1MDgsOTQyMTg2NDEsOTQyMDMwMTksNDcwODQzMDRCAk1Y&skid=e209c23a-0bf3-424c-a9af-b80726d5dfde&g_st=iw" target="_blank">Ver en Google Maps</a></p>
+        <p><strong>Inicio de ceremonia:</strong> 05:30 PM</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="codigo_vestimenta">
+    <h2 class="titulo_roboto">CÓDIGO DE VESTIMENTA</h2>
+    <h2 class="descripcion_sermo">Coctail/Formal</h2>
+    <ul class="listado_vestimenta">
+      <li class="vestimenta_hombre">Hombre: Traje sin necesidad de corbata (aunque es aceptada).</li>
+      <li class="vestimenta_mujer">Mujer: Vestido corto o midi elegante.</li>
+    </ul>
+  </section>
+
+  <!-- Confirmación -->
+  <section class="confirmacion">
+    <button onclick="confirmarAsistencia()">Confirmar Asistencia</button>
+  </section>
+</div>
+
+   <!-- 1. Carga la librería -->
+  <script src="https://cdn.jsdelivr.net/npm/amplitudejs@5.3.2/dist/amplitude.min.js"></script>
+  <!-- 2. Tu código -->
+  <script src="js/index.js"></script>
+  <!-- 3. Resto de librerías -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,127 @@
+let btn = null
+let status = null
+let audio = null
+let swiper = null
+let video = null
+let modal = null
+let contenido = null
+let imgAudio = null
+
+document.addEventListener("DOMContentLoaded", function () {
+  btn = document.getElementById('btnPlayPause');
+  status = document.getElementById('statusAudio');
+  audio = document.getElementById('musica_boda');
+  imgAudio = document.getElementById('imgAudio');
+  Amplitude.init({
+    songs: [
+      {
+        name: "Música de Boda",
+        url: "http://192.168.1.217:443/BETSAM/front/source/music/musica_v2.mp3"
+      }
+    ]
+  });
+  swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+  });
+
+  // Agrega comportamiento de voltear al hacer clic
+  document.querySelectorAll('.flip-card').forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped');
+    });
+  });
+  video = document.getElementById("carta-video");
+  modal = document.getElementById("carta-modal");
+  contenido = document.getElementById("contenido-principal");
+
+  window.addEventListener('click', function activarMusica() {
+    
+    //audio.play().catch(err => console.log("Reproducción bloqueada:", err));
+    // Solo la primera vez
+    //window.removeEventListener('click', activarMusica);
+  });
+
+  
+  btn.addEventListener('click', () => {
+    try{
+      //alert(audio.paused)
+      if (audio.paused) {
+        audio.play();
+        imgAudio.src = 'source/icons/pause.svg';
+        //status.innerText = 'Reproduciendo...';
+      } else {
+        audio.pause();
+        imgAudio.src = 'source/icons/play.svg';
+        //status.innerText = 'Música detenida';
+      }
+    }catch(ex){
+      alert(ex)
+    }
+  });
+
+  // Fecha límite (por ejemplo: 25 de julio de 2025 a las 15:00)
+  const fechaLimite = new Date("2025-09-20T17:30:00").getTime();
+
+  const diasElem = document.getElementById("dias");
+  const horasElem = document.getElementById("horas");
+  const minutosElem = document.getElementById("minutos");
+  const segundosElem = document.getElementById("segundos");
+
+  const intervalo = setInterval(() => {
+    const ahora = new Date().getTime();
+    const diferencia = fechaLimite - ahora;
+
+    if (diferencia <= 0) {
+      clearInterval(intervalo);
+      diasElem.textContent = "00";
+      horasElem.textContent = "00";
+      minutosElem.textContent = "00";
+      segundosElem.textContent = "00";
+      return;
+    }
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    diasElem.textContent = String(dias).padStart(2, '0');
+    horasElem.textContent = String(horas).padStart(2, '0');
+    minutosElem.textContent = String(minutos).padStart(2, '0');
+    segundosElem.textContent = String(segundos).padStart(2, '0');
+  }, 1000);
+
+});
+function prueba(){
+    audio.play().catch(err => alert("Audio bloqueado:", err));
+    video.play();
+    setTimeout(() => {
+      modal.classList.add("ocultar");
+      setTimeout(() => {
+        modal.style.display = "none";
+        contenido.style.display = "flex";
+        
+      }, 400);
+    }, 1400)
+  }
+
+
+// Confirmación
+function confirmarAsistencia() {
+  alert("¡Gracias por confirmar tu asistencia! ❤️");
+}
